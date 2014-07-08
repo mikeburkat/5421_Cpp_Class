b9f6e089b3d90078a16f3bfdb591d16e375c462b
mikeburkat <mike.burkat@gmail.com>
2014-07-08 12:34:25 -0400

    removed main from a2

--- src_A2/wordListDriver.cpp
+++ src_A2/wordListDriver.cpp
@@ -29,19 +29,3 @@ using namespace std;
 //
 //	return 0;
 //}
-int main(void) {
-	WordList wordlist("Seuss.txt");
-
-	wordlist.print(cout);
-	wordlist.print(cout, "Wh");
-	wordlist.print(cout, "the");
-
-	ofstream fout("SeussWordList.txt");
-	assert(fout.good());
-	wordlist.print(fout);
-	wordlist.print(fout, "wh");
-	wordlist.print(fout, "the");
-	fout.close();
-
-	return 0;
-}
